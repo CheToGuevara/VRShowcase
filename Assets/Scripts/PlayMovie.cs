e02d0683ae264c1d5e17812becfa802917c9e413
Aaron Sujar <Aaron Sujar>
2017-02-06 21:59:57 +0100

    feat: Cinema stage almost ready

---
+++ Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMovie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+   
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            Renderer r = GetComponent<Renderer>();
+            MovieTexture movie = (MovieTexture)r.material.mainTexture;
+
+            if (movie.isPlaying)
+            {
+                movie.Pause();
+            }
+            else
+            {
+                movie.Play();
+            }
+        }
+    }
+
+}
